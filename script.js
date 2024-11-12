4f8af71e4d0ff81f1a2258188627daeac86bb68e
Addis Rofik Erlangga <rofikaddis@gmail.com>
2024-11-12 21:04:09 +0700

    menambahkan fungsi tombol reset untuk mengosongkan papan dan reset giliran

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let turntext = document.getElementById('turntext');
 let btns = document.querySelectorAll('.btn');
 let resetbtn = document.getElementById('resetbtn');
+
+let turn = true;
+
+// Reset tombol ketika tombol reset diklik
+resetbtn.addEventListener('click', () => {
+    btns.forEach((button) => {
+        button.innerHTML = "";
+        button.disabled = false;
+    });
+    turntext.innerText = "TURN FOR O";
+    turn = true;
+});
